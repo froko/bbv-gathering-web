a5a3c4327bf564c3697ef1195f775267c3d80465
Patrick Ineichen <github@frokonet.ch>
2019-06-05 13:29:35 +0200

    Make first test green

--- ui-testing/exercise-app/cypress/integration/spec.ts
+++ ui-testing/exercise-app/cypress/integration/spec.ts
@@ -3,11 +3,7 @@ describe('Hello Angular', () => {
     cy.visit('http://localhost:4200');
   });
 
-  it('should display welcome message', () => {
-    cy.get('app-root h1').should('contain', 'Welcome');
-  });
-
-  it('has 3 links', () => {
-    cy.get('app-root li a').should('have.length', 3);
+  it('should display product name', () => {
+    cy.get('[data-cy=product]').should('contain', 'S.H.I.E.L.D.');
   });
 });
